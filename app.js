b12f99d40bee91fedbadc2b122d2f2c7c73a95ce
esubalew-gosaye <mesaye2010@gmail.com>
2023-10-29 19:57:02 +0300

    movie field updated and routes added.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express =    require('express')
-const bodyParser = require('body-parser')
-const multer =     require('multer')
-const connection = require('./helper/mysqldb_conn')
-const path =       require("path")
-const acc =        require("./controller/acccount")
-const mv =         require('./controller/movies')
+require("dotenv").config()
+const express =          require('express')
+const bodyParser =       require('body-parser')
+const multer =           require('multer')
+const connection =       require('./helper/mysqldb_conn')
+const path =             require("path")
+const acc =              require("./controller/acccount")
+const mv =               require('./controller/movies')
+const session =          require('express-session')
+const flush =            require('connect-flash')
 
 const diskStorage = multer.diskStorage({
     destination: function(req, file, cb){
@@ -18,6 +21,12 @@ const diskStorage = multer.diskStorage({
 const upload = multer({storage: diskStorage})
 const app =    express()
 
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flush())
 app.use(express.static(path.join(__dirname, "public")))
 app.set("view engine", "ejs")
 
@@ -115,34 +124,86 @@ app.get('/update/:id', upload.none(), async (req, res) => {
         }
     })
 })
-app.post('/create', upload.single('cover'), async (req, res) => {
+app.post('/create', upload.none(), (req, res) => {
     form_data = req.body
     file = req.file
 
-    if(form_data.name && form_data.year && form_data.type && file){
+    if(form_data.title && form_data.date && form_data.genre ){
         connection.query("SELECT MAX(id) as id FROM movies" , function(err, result){
             let find_id = result[0].id === null ? 100 : result[0].id + 1
             
-            let sql = "INSERT  INTO movies (id, name, type, year, cover) VALUES (?, ?, ?, ?, ?)"
-            connection.query(sql, [find_id, form_data.name, form_data.type, form_data.year, file.filename], function(err, result){
+            let sql = "INSERT  INTO movies (id, title, genre, price, date, poster) VALUES (?, ?, ?, ?, ?)"
+            connection.query(sql, [find_id, form_data.title, form_data.genre, form_data.price, form_data.date, file.filename], function(err, result){
                 if(err){
-                    res.render("upload", {message: 'Movie is not inserted', id: find_id, error: err})
+                    res.render("upload_movie", {message: 'Movie is not inserted', id: find_id, error: err})
                 }else{
-                    res.render("list", {message: "Movie successfully inserted", result: result})
-                }
+                    res.render("index", {message: "Movie successfully inserted", result: result})
+                } 
             })
         })
     }else{
-        res.render("upload",{message: 'Movie is not inserted', id: find_id, error: err})
+        req.flash('success', 'Movie is not inserted')
+        req.flash('error', 'Please provide all details')
+        res.redirect("upload")
     }
 })
 app.get('/login', function(req, res){
-    res.render("login")
+    res.render("login", {message: req.flush("success")})
 })
-
+app.post("/login", upload.none(), (req, res) => {
+   let data = req.body
+   let sqlQuery = "SELECT * FROM users WHERE email= ? and password= ?"
+   connection.query(sqlQuery, [data.email, data.password], function(err, results){
+    if(err){
+        res.redirect(login)
+    }else{
+        if(results.length >= 1){
+            req.session.userId = results[0].id
+            req.session.full_name = results[0].full_name
+            req.flush("success", "User successfully logged in")
+            req.flush("success", "Please find your suitable video from the list")
+            res.redirect("movies")
+        }else{
+            req.flush("error", "User Doesn't logged in")
+            req.flush("success", "Please find your suitable video from the list")
+            res.redirect("login")
+        }
+    }
+   })
+//    connection.query(sqlQuery, [data.email, data.password], function(err, result){
+//         if(!err){
+//             if(result[0] >= 1){
+//                 res.session.user = result
+//                 res.json({dud: result + "sucesss"})
+//                 res.json({dud: 'movies'})
+//             }else{
+//                 res.json({dud: "user doesnt find"})
+//                 res.json({dud: 'login'})
+//             }
+//         }else{
+//             res.json({dud: data.email})
+//             res.json({dud: data.password})
+//             res.json({dud: result})
+//         }
+//    })
+   
+})
 app.get('/register', function(req, res){
     res.render("register")
 })
+
+app.post("/register", (req, res) => {
+    let data = req.body
+    let sqlQuery = "INSERT INTO users(full_name, email, password) values('" + data.full_name + "', '"+ data.email+ "', '"+ data.password+"')"
+    connection.query(sqlQuery, function(err, result){
+        if(!err){
+            res.redirect(200, "movies")
+        }else{
+            res.redirect('register')
+        }
+    })
+ })
+
 app.get('/upload', function(req, res){
     res.render("upload_movie")
 })
@@ -150,9 +211,6 @@ app.get('/upload', function(req, res){
 app.get('/admin/list_movies', (req, res) => {
     res.redirect('/movies')
 })
-
-
-
 
 
 app.use((req, res, next) => {
